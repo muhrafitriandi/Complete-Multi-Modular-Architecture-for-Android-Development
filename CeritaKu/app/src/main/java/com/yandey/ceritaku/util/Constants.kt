d108befbf62da34474dbd159a16e5dd26b1e4172
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-07-07 21:03:10 +0700

    Add CLIENT_ID From Atlas
    Enabled Google Provider in MongoDB Atlas

--- CeritaKu/app/src/main/java/com/yandey/ceritaku/util/Constants.kt
+++ CeritaKu/app/src/main/java/com/yandey/ceritaku/util/Constants.kt
@@ -2,4 +2,5 @@ package com.yandey.ceritaku.util
 
 object Constants {
     const val APP_ID = "ceritakuapp-jhpol"
+    const val CLIENT_ID = "73515907929-2gbui4q6ju925l7q0jkotl4k43v0tl9l.apps.googleusercontent.com"
 }
