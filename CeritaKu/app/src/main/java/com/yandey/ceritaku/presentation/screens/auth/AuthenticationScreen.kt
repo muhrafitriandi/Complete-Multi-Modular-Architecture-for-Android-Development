076826e77a1f7605dde62707577e26cf94a290ce
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2023-07-09 19:50:21 +0700

    Transparent Status and Navigation Bar

--- CeritaKu/app/src/main/java/com/yandey/ceritaku/presentation/screens/auth/AuthenticationScreen.kt
+++ CeritaKu/app/src/main/java/com/yandey/ceritaku/presentation/screens/auth/AuthenticationScreen.kt
@@ -1,9 +1,14 @@
 package com.yandey.ceritaku.presentation.screens.auth
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
 import com.stevdzasan.messagebar.ContentWithMessageBar
 import com.stevdzasan.messagebar.MessageBarState
 import com.stevdzasan.onetap.OneTapSignInState
@@ -23,6 +28,10 @@ fun AuthenticationScreen(
     navigateToHome: () -> Unit,
 ) {
     Scaffold(
+        modifier = Modifier
+            .background(color = MaterialTheme.colorScheme.surface)
+            .statusBarsPadding()
+            .navigationBarsPadding(),
         content = {
             ContentWithMessageBar(messageBarState = messageBarState) {
                 AuthenticationContent(
